9b174b4f94fbad55d873e4eb7877345a89b6af80
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-03-09 17:09:00 +0900

    create project

---
+++ src/main/java/net/shmn7iii/wikipedico/Wikipedico.java
@@ -0,0 +1,17 @@
+package net.shmn7iii.wikipedico;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Wikipedico extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
